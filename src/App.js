ef704b7d8f4ddc4224a122b34aade7e1438a3b6b
Heather Nuffer <zzzuzz@gmail.com>
2019-11-15 09:29:37 -0700

    Added routing for displaying welcome page and characters page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Header from "./components/Header.js";
-import CharacterList from './components/CharacterList';
-
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link, Route } from 'react-router-dom';
+import CharacterList from './CharacterList';
+import WelcomePage from './WelcomePage';
 
 export default function Header() {
   return (
+    <div>
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/characters'>Characters</Link>
+
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </header>
+    </div>
   );
 }
